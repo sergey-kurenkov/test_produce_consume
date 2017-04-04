eda4e1dbee434a44384d301347e93e49f0aff80f
Sergey.Kurenkov <Sergey.Kurenkov@billing.ru>
2017-04-04 14:35:11 +0300

    src

--- fast_procons.cpp
+++ fast_procons.cpp
@@ -32,134 +32,84 @@ class msg_queue {
  public:
     msg_queue(unsigned queue_size);
 
-    void resize(unsigned queue_size);
     void publish(const uint8_t*);
     void on_produce_finished();
-    bool read_msg(array<uint8_t ,256>&);
+    bool consume(array<uint8_t ,256>&);
  
  private:
     using unique_lock_t = unique_lock<std::mutex>;
 
     std::vector<array<uint8_t ,256>> msgs_;
     size_t queue_size_;
-    alignas(64) size_t last_published_;
-    alignas(64) size_t last_read_;
-    alignas(64) bool published_is_ahead_;
+    using index_t = long;
+    alignas(64) atomic<index_t> last_published_;
+    alignas(64) atomic<index_t> last_committed_;
+    alignas(64) atomic<index_t> last_read_;
+
     mutable mutex mt_;
     mutable condition_variable cv_;
     
     size_t get_next_index(size_t curr_value) const;
 };
 
-msg_queue msg_queue_g(2);
-
 msg_queue::msg_queue(unsigned queue_size) :  
-	last_published_(std::numeric_limits<size_t>::max()),
-	last_read_(std::numeric_limits<size_t>::max()) {
-    if (queue_size <= 1U) {
-        queue_size += 1;
+	last_published_(-1),
+	last_committed_(-1),
+	last_read_(-1) {
+    if (queue_size < 1U) {
+        queue_size = 1U;
     }
     queue_size_ = queue_size;
     msgs_.resize(queue_size_);
-    published_is_ahead_ = true;
-}
-
-void msg_queue::resize(unsigned queue_size) {
-	// not thread-safe
-	last_published_ = std::numeric_limits<size_t>::max();
-	last_read_ = std::numeric_limits<size_t>::max();
-    if (queue_size <= 1U) {
-        queue_size += 1;
-    }
-    queue_size_ = queue_size;
-    msgs_.resize(queue_size_);
-    published_is_ahead_ = true;
-}
-
-size_t msg_queue::get_next_index(size_t curr_value) const {
-    return curr_value == queue_size_ ? 0 : curr_value + 1;
 }
 
 void msg_queue::publish(const uint8_t* data) {
     size_t nbytes = *data;
 
-    unique_lock_t lk(mt_);
-    while (true) {
-    	if (last_published_ != std::numeric_limits<size_t>::max()) {
-    		if (last_read_ != std::numeric_limits<size_t>::max()) {
-    			if (published_is_ahead_) {
-    				last_published_ = get_next_index(last_published_);
-    				if (last_published_ == 0U) {
-    					published_is_ahead_ = false;
-    				}
-    				break;
-    			} else {
-        			if (last_published_ != last_read_) {
-        				break;
-        			} else {
-        				// have to wait for reading buffer #[last_read_+1]
-        			}
-    			}
-    		} else {
-    			if (last_published_ != queue_size_-1) {
-    				last_published_ = get_next_index(last_published_);
-    				break;
-    			} else {
-    				// have to wait for reading buffer #0
-    			}
-    		}
-    	} else {
-    		last_published_ = 0U;
-    		break;
-    	}
-        cv_.wait(lk);
-    }
-    uint8_t* msg_ptr = &msgs_[last_published_][0];
-    memcpy(msg_ptr, data, nbytes + 1);
-    lk.unlock();
-    cv_.notify_one();
+	index_t last_published = last_published_.fetch_add(1);
+	index_t where_to_publish = last_published + 1;
+	while (true) {
+		if (last_read_ != -1) {
+			if (where_to_publish - last_read_ >= queue_size_) {
+				continue;
+			}
+		} else {
+			if (where_to_publish >= queue_size_) {
+				continue;
+			}
+		}
+		break;
+	}
+	size_t msg_index = where_to_publish % queue_size_;
+	memcpy(&msgs_[msg_index][0], data, nbytes + 1);
+	while (last_committed_ != last_published) {
+		continue;
+	}
+	++last_committed_;
 }
+
+bool msg_queue::consume(array<uint8_t ,256>& msg) {
+	while (true) {
+		if (last_read_ < last_committed_) {
+			++last_read_;
+			auto msg_index = last_read_% queue_size_;
+			msg = msgs_[msg_index];
+			return true;
+		}
+
+		if (running_count_g == 0) {
+			break;
+		}
+	}
+	return false;
+}
+
 
 void msg_queue::on_produce_finished() {
     cv_.notify_one();
 }
 
-bool msg_queue::read_msg(array<uint8_t ,256>& msg) {
-    unique_lock_t lk(mt_);
-    while (true) {
-    	if (last_published_ != std::numeric_limits<size_t>::max()) {
-    		if (last_read_ != std::numeric_limits<size_t>::max()) {
-    			if (last_read_ != last_published_) {
-    				last_read_ = get_next_index(last_read_);
-    				if (last_read_ == 0U) {
-    					published_is_ahead_ = true;
-    				}
-    				break;
-    			} else {
-    				if (running_count_g == 0) {
-    					return false;
-    				} else {
-    					// have to wait for publishing buffer #[last_published_+1]
-    				}
-    			}
-    		} else {
-    			last_read_ = 0U;
-				break;
-    		}
-    	} else {
-    		if (running_count_g == 0) {
-    			return false;
-    		} else {
-    			// have to wait for first publishing
-    		}
-    	}
-        cv_.wait(lk);
-    }
-    msg =  msgs_[last_read_];
-    lk.unlock();
-    cv_.notify_one();
-	return true;
-}
+msg_queue msg_queue_g(100);
 
 /////////////////////////////////////////////////////////////////////////////////
 
@@ -253,7 +203,7 @@ consume()
     array<uint8_t ,256> msg;
     while (true) {
         /////////////////////////////////////////////////////////////////////////////////
-        auto read_res = msg_queue_g.read_msg(msg);
+        auto read_res = msg_queue_g.consume(msg);
         if (!read_res) {
         	break;
         }
@@ -285,8 +235,6 @@ main(int argc, char** argv)
     default_random_engine reng(seed);
     uniform_int_distribution<int> ud;
 
-    msg_queue_g.resize(100);
-
     vector<Producer> producers;
     for (int i = 0; i < n; ++i) {
         producers.push_back(Producer(ud(reng)));
@@ -295,6 +243,8 @@ main(int argc, char** argv)
     for (auto&p : producers) {
         p.start(M);
     }
+
+    //this_thread::sleep_for(chrono::seconds(10000));
 
     auto ts1 = chrono::high_resolution_clock::now();
     auto result = consume();
